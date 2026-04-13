44452979442277ee11fb851ea46348eb4004d74b
Edson Perotoni <perotoni@gmail.com>
2026-04-12 21:24:00 -0300

    corrige a capitalização do nome do usuário na barra lateral de boas-vindas

--- app_vacaria.py
+++ app_vacaria.py
@@ -28,7 +28,7 @@ if st.session_state["authentication_status"]:
     
     # Barra lateral com Boas-vindas e Botão de Sair
     with st.sidebar:
-        st.write(f"## Olá, {st.session_state['name'].capitalize()}!")
+        st.write(f"## Olá, {st.session_state['name'].title()}!")
         authenticator.logout(button_name='Sair do Sistema', location='sidebar')
     
     st.title("🌎 Sistema de Informações Geográficas de Vacaria")
